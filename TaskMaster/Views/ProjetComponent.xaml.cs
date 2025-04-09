a1c174650678f7344c1f885428ee5c304362b1c3
Aymeric Sudre <aymeric2sudre0@gmail.com>
2025-04-09 12:22:14 +0200

    Ajout Front Page accueil transition connexion
    Faire padding/Margin entre les projets
    Les rendre cliquable pour acceder à la page du projet en question

---
+++ TaskMaster/Views/ProjetComponent.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace TaskMaster.Views;
+
+public partial class ProjetComponent : Frame
+{
+    public ProjetComponent()
+    {
+        InitializeComponent();
+    }
+}
